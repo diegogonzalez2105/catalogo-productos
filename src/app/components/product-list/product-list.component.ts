bb163a618643a6dc4cbd111b159bb61211889dde
Diego armando gonzalez burgos <43834725+diegogonzalez2105@users.noreply.github.com>
2026-06-10 21:09:46 -0500

    produc list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { ProductCardComponent } from '../product-card/product-card.component';
+import { SortControlsComponent } from '../sort-controls/sort-controls.component';
+import { loadProducts, setSortConfig } from '../../store/products/products.actions';
+import {
+  selectSortedProducts,
+  selectProductsLoading,
+  selectProductsError
+} from '../../store/products/products.selectors';
+import { SortConfig } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductCardComponent, SortControlsComponent],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  private readonly store = inject(Store);
 
+  products$ = this.store.select(selectSortedProducts);
+  loading$ = this.store.select(selectProductsLoading);
+  error$ = this.store.select(selectProductsError);
+
+  ngOnInit(): void {
+    this.store.dispatch(loadProducts());
+  }
+
+  onSortChange(config: SortConfig | null): void {
+    this.store.dispatch(setSortConfig({ config }));
+  }
 }
