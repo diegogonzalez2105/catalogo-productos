fb722a6319d2347d9727a8cf1400082b6918ec7f
Diego armando gonzalez burgos <43834725+diegogonzalez2105@users.noreply.github.com>
2026-06-10 20:03:08 -0500

    init

--- src/app/components/carousel/carousel.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-carousel',
-  imports: [],
-  templateUrl: './carousel.html',
-  styleUrl: './carousel.scss',
-})
-export class Carousel {}
--- src/app/components/sort-controls/sort-controls.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sort-controls',
-  imports: [],
-  templateUrl: './sort-controls.html',
-  styleUrl: './sort-controls.scss',
-})
-export class SortControls {}
